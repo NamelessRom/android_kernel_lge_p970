c66d7527a986e5a9ea516c1432c4c8cc6848016c
Alexander Martinz <eviscerationls@gmail.com>
2013-11-14 19:46:29 +0100

    add extra free kbytes tunable
    Add a userspace visible knob to tell the VM to keep an extra amount
    of memory free, by increasing the gap between each zone's min and
    low watermarks.
    
    This is useful for realtime applications that call system
    calls and have a bound on the number of allocations that happen
    in any short time period.  In this application, extra_free_kbytes
    would be left at an amount equal to or larger than than the
    maximum number of allocations that happen in any burst.
    
    It may also be useful to reduce the memory use of virtual
    machines (temporarily?), in a way that does not cause memory
    fragmentation like ballooning does.
    
    [ccross]
    Revived for use on old kernels where no other solution exists.
    The tunable will be removed on kernels that do better at avoiding
    direct reclaim.
    
    Change-Id: I184bfa245bfca34282bae669ee990ee21badf6b7

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -95,6 +95,7 @@ extern int suid_dumpable;
 extern char core_pattern[];
 extern unsigned int core_pipe_limit;
 extern int pid_max;
+extern int extra_free_kbytes;
 extern int min_free_kbytes;
 extern int min_free_order_shift;
 extern int pid_max_min, pid_max_max;
@@ -1181,6 +1182,14 @@ static struct ctl_table vm_table[] = {
 	},
 
 #endif /* CONFIG_COMPACTION */
+	{
+		.procname	= "extra_free_kbytes",
+		.data		= &extra_free_kbytes,
+		.maxlen		= sizeof(extra_free_kbytes),
+		.mode		= 0644,
+		.proc_handler	= min_free_kbytes_sysctl_handler,
+		.extra1		= &zero,
+	},
 	{
 		.procname	= "min_free_kbytes",
 		.data		= &min_free_kbytes,
